cd6369a7220fdba24465ea256e5a89cd8dcd9405
Sam Woods <sam@imprint-digital.com>
2023-05-11 13:33:18 -0600

    Build Hero

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "71a455954fae30c7"; }
+/******/ 		__webpack_require__.h = function() { return "f5dc690d9f4e3a69"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -2,15 +2,27 @@ import React from 'react';
 import className from 'classnames/bind';
 import { Heading } from '../../components';
 import styles from './Hero.module.scss';
+import Image from 'next/image';
 
 let cx = className.bind(styles);
 
 export default function Hero({ title, level = 'h1', children, className }) {
   return (
     <div className={cx(['component', className])}>
+      <div className={cx('copy')}>
       <Heading level={level}>
-        <span className={cx('title')}>{title}</span>
+        <span className={cx('title')}>Digital Marketing Solutions That Work</span>
       </Heading>
+      <p className={cx('subtitle')}>Stop getting burned by digital marketing companies. Your digital marketing partner that ensures your brand thrives.</p>
+      </div>
+      <Image
+        className={cx('hero_bull')}
+        src='https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/BULL-ROCKET.gif'
+        alt='For the Moon!'
+        width='480'
+        height='480'
+        priority='priority'
+      />
       {children}
     </div>
   );
