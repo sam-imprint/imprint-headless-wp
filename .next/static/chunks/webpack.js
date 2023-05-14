fb9764e783f4891fddeed45499cb0235763bdb01
Sam Woods <sam@imprint-digital.com>
2023-05-13 23:09:57 -0600

    Build Industry Crards and resize components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e2587ad69f7bdca1"; }
+/******/ 		__webpack_require__.h = function() { return "bf97825c116d37e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Industries/Industries.js
+++ components/Industries/Industries.js
@@ -2,6 +2,7 @@ import React from 'react';
 import className from 'classnames/bind';
 import styles from './Industries.module.scss';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 let cx = className.bind(styles);
@@ -10,6 +11,50 @@ export default function Industries({ className }) {
   return (
     <div className={cx(['component', className])}>
       <div className={cx('copy')}><h2>Who We <span className={cx('headline_red')}>Serve</span></h2></div>
+      <div className={cx('cards_wrap')}>
+        <div className={cx('cards')}>
+        <Link href="/">
+        <a>
+        <Image
+        className={cx('industry_hero')}
+        src='https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/Startups-Thumb-CLIPPED.jpg'
+        alt='Startups'
+        width='332'
+        height='283'
+        />
+        <div className={cx('title')}>Startups</div>
+        </a>
+        </Link>
+        </div>
+        <div className={cx('cards')}>
+        <Link href="/">
+        <a>
+        <Image
+        className={cx('industry_hero')}
+        src='https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/Ecommerce-Thumb-CLIPPED.jpg'
+        alt='E-Commerce'
+        width='332'
+        height='283'
+        />
+        <div className={cx('title')}>E-Commerce</div>
+        </a>
+        </Link>
+        </div>
+        <div className={cx('cards')}>
+        <Link href="/">
+        <a>
+        <Image
+        className={cx('industry_hero')}
+        src='https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/Growth-Thumb-CLIPPED.jpg'
+        alt='Growth Stage'
+        width='332'
+        height='283'
+        />
+        <div className={cx('title')}>Growth Stage</div>
+        </a>
+        </Link>
+        </div>
+      </div>
     </div>
 
   );
