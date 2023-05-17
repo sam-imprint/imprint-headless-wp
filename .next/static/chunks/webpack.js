3745d232d86c04268bf8c81e7c68d09902b131ff
Sam Woods <sam@imprint-digital.com>
2023-05-17 15:00:55 -0600

    Fix Footer Nav Spacing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5fe9e0d51a325086"; }
+/******/ 		__webpack_require__.h = function() { return "c481904a64e8034e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
