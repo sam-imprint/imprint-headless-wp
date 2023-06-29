bd78905c79d9b2c990dd849658e098c4ebf3b17e
Sam Woods <sam@imprint-digital.com>
2023-06-29 12:27:26 -0600

    Style and Brand Contact Modal

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "68da210c2b41683b"; }
+/******/ 		__webpack_require__.h = function() { return "8d1b2ef2f0f94661"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/ContactModal/ContactModal.js
+++ components/ContactModal/ContactModal.js
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 import Link from 'next/link';
 import styles from './ContactModal.module.scss';
 import Modal from 'react-modal';
+import Image from 'next/image';
 
 Modal.setAppElement('#__next') // replace with your app element id
 
@@ -15,30 +16,35 @@ let cx = classNames.bind(styles);
     contentLabel="Contact Modal"
     className={cx('contact_modal')}
   >
-    <button className={cx('modal_close')} onClick={onRequestClose}>Close</button>
-    <h2>Contact Us</h2>
-    <p>Phone: 970-518-4491</p>
-    <p>Address: 815 14th St SW, Loveland CO 80537</p>
-    <form>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Email:
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Business Name:
-        <input type="text" name="businessName" />
-      </label>
-      <label>
-        Phone:
-        <input type="tel" name="phone" />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-    
+    <button className={cx('modal_close')} onClick={onRequestClose}>
+    <Image 
+                src="https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/Black-Form-Arrow.svg"
+                alt="Close Modal"
+                width="342"
+                height="62"
+                />
+    </button>
+        <Link href="/">
+          <a className={cx('logo')}>
+            <Image 
+                src="https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/03/ID-Logo-LG.png"
+                alt="Imprint Digital"
+                width="342"
+                height="62"
+                />
+          </a>
+        </Link>
+    <div className={cx(['info_link'])}>
+    <Link href="tel:+19705184491">970-518-4491</Link>
+    <Link href="http://localhost:3000/">815 14th St SW, Loveland CO 80537</Link>
+    </div>
+    <form action="/send-data-here" method="post">
+            <div className={cx(['field_row'])}><label for="name">Name</label><input type="text" id="name" name="name" /></div>
+            <div className={cx(['field_row'])}><label for="email">Email</label><input type="text" id="email" name="email" /></div>
+            <div className={cx(['field_row'])}><label for="business">Business Name</label><input type="text" id="business" name="business" /></div>
+            <div className={cx(['field_row'])}><label for="phone">Phone</label><input type="text" id="phone" name="phone" /></div>
+            <button type="submit">Get Your Free Consultation</button>
+        </form>
   </Modal>
   );
 }
