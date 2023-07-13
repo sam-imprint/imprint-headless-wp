0be790a4239690277d0d383e9f5605fb3d976585
Sam Woods <sam@imprint-digital.com>
2023-07-13 14:59:50 -0600

    Add pagination to Blog Page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fbaa413d17e76c0d"; }
+/******/ 		__webpack_require__.h = function() { return "e1cef278ebcf197e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/blog.js
+++ pages/blog.js
@@ -14,40 +14,79 @@ import {
 } from '../components';
 import { getNextStaticProps } from '@faustwp/core';
 import Image from 'next/image';
-
+import React, { useState, useEffect } from 'react';
 
 export default function Page(props) {
-  const { data } = useQuery(Page.query, {
-    variables: Page.variables(),
+  const [cursor, setCursor] = useState(null);
+  const { loading, error, data, fetchMore } = useQuery(Page.query, {
+    variables: { ...Page.variables(), first: 10, after: cursor },
+    notifyOnNetworkStatusChange: true,
   });
-  const title = props.title;
 
-  const { title: siteTitle, description: siteDescription } = data?.generalSettings;
-  const primaryMenu = data?.headerMenuItems?.nodes ?? [];
-  const footerMenu = data?.footerMenuItems?.nodes ?? [];
+  const loadMorePosts = (direction) => {
+    let newCursor;
+    if (direction === 'next') {
+      newCursor = data.posts.pageInfo.endCursor;
+    } else if (direction === 'prev') {
+      newCursor = data.posts.pageInfo.startCursor;
+    }
+
+    setCursor(newCursor);
+  };
+
+  useEffect(() => {
+    if (cursor) {
+      fetchMore({
+        variables: {
+          after: cursor,
+        },
+        updateQuery: (prev, { fetchMoreResult }) => {
+          if (!fetchMoreResult) return prev;
+          return fetchMoreResult;
+        },
+      });
+    }
+  }, [cursor, fetchMore]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const title = props.title || 'Default Title';
+
+  const { title: siteTitle, description: siteDescription } = data.generalSettings;
+  const primaryMenu = data.headerMenuItems.nodes ?? [];
+  const footerMenu = data.footerMenuItems.nodes ?? [];
 
   return (
     <>
       <SEO title={siteTitle} description={siteDescription} />
       <Header
-        title={siteTitle}
+        title={title}
         description={siteDescription}
         menuItems={primaryMenu}
       />
       <Main>
-      <ArchiveHeader title='BLog'/>
-          <Container>
-            {data.posts.nodes.map((post) => (
-              <Excerpt
-                title={post.title}
-                excerpt={post.excerpt}
-                date={post.date}
-                author={post.author?.node.name}
-                uri={post.uri}
-                featuredImage={post.featuredImage?.node}
-              />
-            ))}
-          </Container>
+        <ArchiveHeader title='Blog'/>
+        <Container>
+          {data.posts.nodes.map((post) => (
+            <Excerpt
+              title={post.title}
+              excerpt={post.excerpt}
+              date={post.date}
+              author={post.author?.node.name}
+              uri={post.uri}
+              featuredImage={post.featuredImage?.node}
+            />
+          ))}
+          <div>
+            <button onClick={() => loadMorePosts('prev')} disabled={!data.posts.pageInfo.hasPreviousPage}>
+              Previous
+            </button>
+            <button onClick={() => loadMorePosts('next')} disabled={!data.posts.pageInfo.hasNextPage}>
+              Next
+            </button>
+          </div>
+        </Container>
       </Main>
       <Footer title={siteTitle} menuItems={footerMenu} />
     </>
@@ -61,17 +100,25 @@ Page.query = gql`
   query GetPageData(
     $headerLocation: MenuLocationEnum
     $footerLocation: MenuLocationEnum
+    $first: Int
+    $after: String
   ) {
-    posts {
-        nodes {
-          id
-          title
-          excerpt
-          date
-          uri
-          ...FeaturedImageFragment
-        }
+    posts(first: $first, after: $after) {
+      pageInfo {
+        endCursor
+        startCursor
+        hasNextPage
+        hasPreviousPage
       }
+      nodes {
+        id
+        title
+        excerpt
+        date
+        uri
+        ...FeaturedImageFragment
+      }
+    }
     generalSettings {
       ...BlogInfoFragment
     }
@@ -91,7 +138,9 @@ Page.query = gql`
 Page.variables = () => {
   return {
     headerLocation: MENUS.PRIMARY_LOCATION,
-    footerLocation: MENUS.FOOTER_LOCATION
+    footerLocation: MENUS.FOOTER_LOCATION,
+    first: 10,
+    after: null,
   };
 };
 
