65ab70bf2a71a5da3665396186ac540957d41a9f
Sam Woods <sam@imprint-digital.com>
2023-07-19 09:09:41 -0600

    Update Archive Header Mobile Styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "205b86784dca314b"; }
+/******/ 		__webpack_require__.h = function() { return "248366a2d3937fd4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
