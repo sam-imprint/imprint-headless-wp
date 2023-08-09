f1b66e506d4cb8c7718eceb185a521db45b3f320
Sam Woods <sam@imprint-digital.com>
2023-08-09 10:26:22 -0600

    Update Case Study Query

--- components/CaseStudy/CaseStudy.js
+++ components/CaseStudy/CaseStudy.js
@@ -6,34 +6,47 @@ import Link from 'next/link';
 
 let cx = className.bind(styles);
 
-let studies = [
-    {image:"https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/flowers-cut-SPEC-380-640.png",
-    logo:"https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/earles-logo-SPEC-177-108.png",
-    title:"Earle's Florist & Gifts",
-    copy:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper ex a consequat posuere.",
-    link:"/",
-    data:"+20.42%",
-    metric:"End of Year Revenue",
-    },
-    {image:"https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/vintage-willows-flower-dress-cutout-SPEC-380-640.png",
-    logo:"https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/vintage-willows-SPEC-177-108.png",
-    title:"Earle's Florist & Gifts",
-    copy:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper ex a consequat posuere.",
-    link:"/",
-    data:"+20.42%",
-    metric:"End of Year Revenue",
-    },
-    {image:"https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/noco-jj-instructor-SPEC-380-640.png",
-    logo:"https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/noco-jj-logo-SPEC-177-108.png",
-    title:"Earle's Florist & Gifts",
-    copy:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper ex a consequat posuere.",
-    link:"/",
-    data:"+20.42%",
-    metric:"End of Year Revenue",
+
+
+const GET_CASE_STUDY_ARCHIVE = gql`
+query GetCaseStudy {
+  caseStudies {
+    nodes {
+      title
+      uri
+      caseStudies {
+        caseExcerpt
+        caseHeroImage {
+          altText
+          sourceUrl
+        }
+        caseMetric1
+        caseMetric1Label
+        caseMetric2
+        caseMetric2Label
+        caseMetric3
+        caseMetric3Label
+        caseMetric4
+        caseMetric4Label
+        caseWhitePaper {
+          sourceUrl
+        }
+        companyLogo {
+          sourceUrl
+          altText
+        }
+      }
     }
-]
+  }
+}
+`;
 
 export default function CaseStudy({ className }) {
+
+  const { loading, error, data } = useQuery(GET_CASE_STUDY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
     const backgroundImagesRef = useRef(Array(studies.length).fill('https://sbx-dev.imprint-digital.com/wp-content/uploads/2023/05/case-study-bg.svg'));
 
@@ -60,15 +73,16 @@ export default function CaseStudy({ className }) {
           return () => window.removeEventListener('scroll', handleScroll);
         }
       }, []); // empty dependency array to add the listener only once
-  
 
-    const studyComponents= studies.map((study, index) => (
-    <div className={cx('study_wrap')} >
+  return (
+    <div className={cx(['component', className])}>
+      {data.caseStudies.nodes.map((node, index) => (
+        <div className={cx('study_wrap')} >
         <div className={cx('image_wrap', `bg_scroll${index}`)} key={index}>
         <Image
         className={cx(['study_image'])}
-        src={study.image}
-        alt='Our Work'
+        src={node.caseHeroImage.sourceUrl}
+        alt={node.caseHeroImage.altText}
         width='380'
         height='640'
         />
@@ -77,42 +91,38 @@ export default function CaseStudy({ className }) {
             <div className={cx('study_hero')}>
             <Image
             className={cx(['study_logo'])}
-            src={study.logo}
-            alt='Find us!'
+            src={node.companyLogo.sourceUrl}
+            alt={node.companyLogo.altText}
             width='177'
             height='108'
             />
             <div className={cx('study_copy')}>
-                <h3>{study.title}</h3>
-                <p>{study.copy}</p>
-                <Link href={study.link}><span className={cx('cta_link')}>White Paper</span></Link>
+                <h3>{node.title}</h3>
+                <p>{node.caseExcerpt}</p>
+                <Link href={node.caseWhitePaper.sourceUrl}><span className={cx('cta_link')}>White Paper</span></Link>
             </div>
             </div>
             <div className={cx('study_stats')}>
                 <div className={cx('stat')}>
-                    <div className={cx('data')}>{study.data}</div>
-                    <div className={cx('metric')}>{study.metric}</div>
+                    <div className={cx('data')}>{node.caseMetric1}</div>
+                    <div className={cx('metric')}>{node.caseMetric1Label}</div>
                 </div>
                 <div className={cx('stat')}>
-                    <div className={cx('data')}>{study.data}</div>
-                    <div className={cx('metric')}>{study.metric}</div>
+                    <div className={cx('data')}>{node.caseMetric2}</div>
+                    <div className={cx('metric')}>{node.caseMetric2Label}</div>
                 </div>
                 <div className={cx('stat')}>
-                    <div className={cx('data')}>{study.data}</div>
-                    <div className={cx('metric')}>{study.metric}</div>
+                    <div className={cx('data')}>{node.caseMetric3}</div>
+                    <div className={cx('metric')}>{node.caseMetric3Label}</div>
                 </div>
                 <div className={cx('stat')}>
-                    <div className={cx('data')}>{study.data}</div>
-                    <div className={cx('metric')}>{study.metric}</div>
+                    <div className={cx('data')}>{node.caseMetric4}</div>
+                    <div className={cx('metric')}>{node.caseMetric4Label}</div>
                 </div>
             </div>
         </div>
         </div>
-    ));
-
-  return (
-    <div className={cx(['component', className])}>
-        {studyComponents}
+      ))}
     </div>
 
   );
